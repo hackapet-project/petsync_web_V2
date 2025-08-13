c7562d8ce7510b7764abcbc75f9ba36483687469
drejon <drc.working@gmail.com>
2025-08-13 11:12:28 +0200

    Add auth urls to api

--- back/api/v1/urls.py
+++ back/api/v1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include #type: ignore
 
 urlpatterns = [
     path('users/', include('api.v1.users.urls')),
+    path('auth/', include('api.v1.auth.urls')),
     # path('users/', include('users.urls'))
 ]
