aaedddb22ea7edd8184ea80c62604dd2004a302f
drejon <drc.working@gmail.com>
2025-09-26 10:36:34 +0200

    Add shelter foreign key

--- back/api/v1/tests/auth/permissions/permissions_test.py
+++ back/api/v1/tests/auth/permissions/permissions_test.py
@@ -2,7 +2,7 @@ import pytest #type: ignore
 
 class TestPermissions():
 
-  @pytest.mark.django.db
+  @pytest.mark.django_db
   def test_admins_has_full_access(self, user_factory):
     admin = user_factory()
     assert True == True
