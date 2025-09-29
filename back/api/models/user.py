328807dd02f5ccb41be020c00a93e6536fdd193f
Diego Rejón <drc.working@gmail.com>
2025-09-29 16:13:40 +0200

    Add staff boolean field

--- back/api/models/user.py
+++ back/api/models/user.py
@@ -56,6 +56,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     shelter = models.ForeignKey(
