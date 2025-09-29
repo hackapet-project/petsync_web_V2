e3d201adda0dcd8231e77d3864f15190eefeed75
Diego <drc.working@gmail.com>
2025-09-29 19:37:55 +0200

    Refactor user model

--- back/api/models/user.py
+++ back/api/models/user.py
@@ -23,11 +23,11 @@ class UserQuerySet(models.QuerySet):
 class CustomUserManager(BaseUserManager.from_queryset(UserQuerySet)):
     user_in_migrations = True
 
-    def create_user(self, email, shelter, password=None, name=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
-        if shelter is None:
-            raise ValueError("Users must belong to a shelter")
+        # if shelter is None:
+        #     raise ValueError("Users must belong to a shelter")
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
@@ -35,13 +35,13 @@ class CustomUserManager(BaseUserManager.from_queryset(UserQuerySet)):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, shelter, name=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         extra_fields.setdefault("role", "ADMIN")
 
-        return self.create_user(email=email, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser):
     user_id = models.CharField(max_length=26, default=generate_ulid, editable=False, unique=True)
