b47c2b794b7e4599165080d1b987ea0564ad6573
Diego <drc.working@gmail.com>
2025-10-02 14:53:35 +0200

    Add dashboard route

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+// import { Dashboard } from './pages/shelter/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+    loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent),
     title: 'Iniciar Sesión - PetSync'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/shelter/dashboard/dashboard').then(m => m.Dashboard),
+    title: 'Dashboard - PetSync'
   },
   {
     path: '',
