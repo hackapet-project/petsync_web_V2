854dfcd91b5f75a03f9909bc17a4550dc63f42d5
Diego <drc.working@gmail.com>
2025-10-03 14:49:30 +0200

    clean dashboard component

--- front/src/app/pages/shelter/dashboard/dashboard.ts
+++ front/src/app/pages/shelter/dashboard/dashboard.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGeneratorComponent } from '../../../components/form-generator/form-generator';
-import { FormConfig } from '../../../components/form-generator/interfaces';
-import { loginFormConfig } from '../../../components/form-generator/forms_scaffolders/login';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [
-    FormGeneratorComponent
-  ],
+  imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
-  myFormConfig: FormConfig = loginFormConfig
-
-  handleFormSubmission($event: Event) {
-    console.log('Submited from custom form generator')
-  }
 }
