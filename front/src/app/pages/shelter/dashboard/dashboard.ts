2a2bb46af4e36f305a526759dbdd00f8c8392a86
Diego Rejón <drc.working@gmail.com>
2025-10-04 22:43:50 +0200

    Modify dashboard page

--- front/src/app/pages/shelter/dashboard/dashboard.ts
+++ front/src/app/pages/shelter/dashboard/dashboard.ts
@@ -1,11 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { Nav } from '../../../components/nav/nav';
+import { Dashboard as Dash } from '../../../components/dashboard/dashboard';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    Nav,
+    Dash,
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
