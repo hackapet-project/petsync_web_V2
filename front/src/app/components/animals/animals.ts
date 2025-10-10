8339fbc446f7ff2f451a9bea0b8224182dcfb35d
Diego Rejón <drc.working@gmail.com>
2025-10-10 17:34:46 +0200

    Show animals grid

--- front/src/app/components/animals/animals.ts
+++ front/src/app/components/animals/animals.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Animal, animals } from '../../core/utils/animal_mocks';
+import { AnimalCard } from '../widgets/animal-card/animal-card';
 
 @Component({
   selector: 'app-animals',
-  imports: [],
+  imports: [
+    AnimalCard
+  ],
   templateUrl: './animals.html',
   styleUrl: './animals.css'
 })
 export class Animals {
+  public animals: Animal[] = animals;
 
 }
