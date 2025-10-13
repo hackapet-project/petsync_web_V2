d97979b9b1710686d1382e8aa4a2e258388e2fc2
Diego Rejón <drc.working@gmail.com>
2025-10-13 13:30:13 +0200

    Add filter nav

--- front/src/app/components/animals/animals.ts
+++ front/src/app/components/animals/animals.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Animal, animals } from '../../core/utils/animal_mocks';
 import { AnimalCard } from '../widgets/animal-card/animal-card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-animals',
   imports: [
-    AnimalCard
+    AnimalCard,
+    MatIcon,
   ],
   templateUrl: './animals.html',
   styleUrl: './animals.css'
