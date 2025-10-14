4e8f71a355cd90487fe46efb4c503e84c8499e67
Diego Rejón <drc.working@gmail.com>
2025-10-14 16:45:55 +0200

    create adoptions component

---
+++ front/src/app/components/adoptions/adoptions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adoptions',
+  imports: [],
+  templateUrl: './adoptions.html',
+  styleUrl: './adoptions.css'
+})
+export class Adoptions {
+
+}
